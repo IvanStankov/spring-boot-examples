905c4a5642cb8cb0097c312e995215afd9e3bef8
IvanStankov <ivan.stankov1992@gmail.com>
2018-07-03 10:47:13 +0600

    Created multi module project

--- hello-world/src/main/java/com/ivan/springbootexamples/helloworld/Application.java
+++ hello-world/src/main/java/com/ivan/springbootexamples/helloworld/Application.java
@@ -1,4 +1,4 @@
-package com.ivan.springbootexamples;
+package com.ivan.springbootexamples.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- hello-world/src/main/java/com/ivan/springbootexamples/helloworld/HelloController.java
+++ hello-world/src/main/java/com/ivan/springbootexamples/helloworld/HelloController.java
@@ -1,4 +1,4 @@
-package com.ivan.springbootexamples;
+package com.ivan.springbootexamples.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
