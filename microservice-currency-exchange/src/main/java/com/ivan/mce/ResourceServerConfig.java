df11bcd829a1fed39c48d290d508abdc90115b20
IvanStankov <ivan.stankov1992@gmail.com>
2018-07-07 15:22:21 +0600

    Added token relay

--- microservice-currency-exchange/src/main/java/com/ivan/mce/ResourceServerConfig.java
+++ microservice-currency-exchange/src/main/java/com/ivan/mce/ResourceServerConfig.java
@@ -1,19 +1,47 @@
 package com.ivan.mce;
 
+import feign.RequestInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 
 @Configuration
 @EnableResourceServer
 @EnableOAuth2Sso
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+    private static final Logger logger = LoggerFactory.getLogger(ResourceServerConfig.class);
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
         resources.resourceId("mce");
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
+    }
+
+    @Bean
+    public RequestInterceptor requestTokenBearerInterceptor() {
+        return requestTemplate -> {
+            OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails)
+                    SecurityContextHolder.getContext().getAuthentication().getDetails();
+
+            logger.info("================= token - {}", details.getTokenValue());
+
+            requestTemplate.header("Authorization", "bearer " + details.getTokenValue());
+        };
     }
 
 }
