ae9e1a8f822072720a7fd77dfbab40945e7f14a7
IvanStankov <ivan.stankov1992@gmail.com>
2018-07-09 01:03:47 +0600

    Zuul authentication relay, simple filter

---
+++ microservice-zuul/src/main/java/com/ivan/mz/filter/SimpleFilter.java
@@ -0,0 +1,40 @@
+package com.ivan.mz.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class SimpleFilter extends ZuulFilter {
+    private static final Logger logger = LoggerFactory.getLogger(SimpleFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        final RequestContext ctx = RequestContext.getCurrentContext();
+        final HttpServletRequest request = ctx.getRequest();
+
+        logger.info("------------ {}", request.getRequestURI());
+
+        return null;
+    }
+}
