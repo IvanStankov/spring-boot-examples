8dc9266a652cd60312e9291e47c1b388eab1efd6
IvanStankov <ivan.stankov1992@gmail.com>
2018-07-11 18:53:02 +0600

    Opened actuator endpoints

--- microservice-forex/src/main/java/com/ivan/mf/controller/ResourceServerConfig.java
+++ microservice-forex/src/main/java/com/ivan/mf/controller/ResourceServerConfig.java
@@ -3,6 +3,7 @@ package com.ivan.mf.controller;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.JwtAccessTokenConverterConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
@@ -19,6 +20,13 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
         resources.resourceId("mf");
+    }
+
+    @Override
+    public void configure(final HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/actuator/**").permitAll()
+                .antMatchers("/**").authenticated();
     }
 
     @Bean
